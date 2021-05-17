47229ea15d08044b57a795f44c31c5b2e46afe07
i-play-lf2 <914637118@qq.com>
2021-05-17 19:53:17 +0800

    draft

--- draft/foo.ts
+++ draft/foo.ts
@@ -1,3 +1,9 @@
-import { test } from "lambda-calculus";
+import { $n, mult, plus, pow, pred_, toNumber } from "lambda-calculus";
 
-console.log(test());
+const $4 = $n(4);
+const $2 = $n(2);
+
+console.log(toNumber(plus($4, $2)));
+console.log(toNumber(mult($4, $2)));
+console.log(toNumber(pow($4, $2)));
+console.log(toNumber(pred_($4)));
