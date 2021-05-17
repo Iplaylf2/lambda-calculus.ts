a0194ba26611a0f0e83148042fc7d9e109ae824f
i-play-lf2 <914637118@qq.com>
2021-05-17 19:49:18 +0800

    export numeral

--- src/lambda-calculus.ts
+++ src/lambda-calculus.ts
@@ -8,3 +8,13 @@ export {
   toNative as toBoolean,
 } from "./boolean";
 export { $Pair, $pair, first, second, toNative as toPair } from "./pair";
+export {
+  $Numeral,
+  $n,
+  succ,
+  plus,
+  mult,
+  pow,
+  pred_,
+  toNative as toNumber,
+} from "./numeral";
