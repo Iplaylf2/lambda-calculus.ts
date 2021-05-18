6ee03b4de7ff6feb7b9038f64867995a992b715c
i-play-lf2 <914637118@qq.com>
2021-05-18 11:58:47 +0800

    format

--- src/boolean.ts
+++ src/boolean.ts
@@ -1,17 +1,17 @@
-// (true a b) = a;
-// (false a b) = b;
-// ƛ x. ƛ a b.(x a b)
+//  (true a b) = a;
+//  (false a b) = b;
+//  ƛ x. ƛ a b.(x a b)
 
 export interface $Boolean {
   <T, K>(a: T, b: K): T | K;
 }
 
-// true = ƛ a b. a
+//  true = ƛ a b. a
 export function $true<T>(a: T, _b: any): T {
   return a;
 }
 
-// false = ƛ a b. b
+//  false = ƛ a b. b
 export function $false<T>(_a: any, b: T): T {
   return b;
 }
@@ -20,17 +20,17 @@ export function toNative(x: $Boolean): boolean {
   return x(true, false);
 }
 
-// and = ƛ a b. (a b false)
+//  and = ƛ a b. (a b false)
 export function and(a: $Boolean, b: $Boolean): $Boolean {
   return a(b, $false);
 }
 
-// or = ƛ a b. (a true b)
+//  or = ƛ a b. (a true b)
 export function or(a: $Boolean, b: $Boolean): $Boolean {
   return a($true, b);
 }
 
-// not = ƛ x. (x false true)
+//  not = ƛ x. (x false true)
 export function not(x: $Boolean): $Boolean {
   return x($false, $true);
 }
--- src/numeral.ts
+++ src/numeral.ts
@@ -1,19 +1,19 @@
 import { $true, $false, $Boolean } from "./boolean";
 import { $pair, $Pair, first, second } from "./pair";
 
-// 0 = ƛ f x. x
-// 1 = ƛ f x. (f x)
+//  0 = ƛ f x. x
+//  1 = ƛ f x. (f x)
 
 export interface $Numeral {
   <T>(f: (x: T) => T, x: T): T;
 }
 
-// 0 = ƛ f x. x
+//  0 = ƛ f x. x
 export function $0<T>(_f: (x: T) => T, x: T): T {
   return x;
 }
 
-// 1 = ƛ f x. x
+//  1 = ƛ f x. x
 export function $1<T>(f: (x: T) => T, x: T): T {
   return f(x);
 }
@@ -32,43 +32,43 @@ export function toNative(x: $Numeral): number {
   return x((x) => x + 1, 0);
 }
 
-// successor =  ƛ n. ƛ f x. (f (n f x))
+//  successor =  ƛ n. ƛ f x. (f (n f x))
 export function succ(n: $Numeral): $Numeral {
   return function (f, x) {
     return f(n(f, x));
   };
 }
 
-// plus = ƛ a b. ƛ f x. (a f (b f x))
+//  plus = ƛ a b. ƛ f x. (a f (b f x))
 export function plus(a: $Numeral, b: $Numeral): $Numeral {
   return function (f, x) {
     return a(f, b(f, x));
   };
 }
 
-// plus = ƛ a b. (a succ b)
+//  plus = ƛ a b. (a succ b)
 export function plus_(a: $Numeral, b: $Numeral): $Numeral {
   return a(succ, b);
 }
 
-// multiple = ƛ a b. ƛ f x. (a <ƛ x. (b f x)> x)
-// =  ƛ a b. ƛ f x. (a (b f) x)
-// =  ƛ a b. ƛ f. (a (b f))
+//  multiple = ƛ a b. ƛ f x. (a <ƛ x. (b f x)> x)
+//  =  ƛ a b. ƛ f x. (a (b f) x)
+//  =  ƛ a b. ƛ f. (a (b f))
 export function mult(a: $Numeral, b: $Numeral): $Numeral {
   return function (f, x) {
     return a((x) => b(f, x), x);
   };
 }
 
-// multiple = ƛ a b. (a <ƛ n. (plus b n)> 0)
-// = (a (plus b) 0)
+//  multiple = ƛ a b. (a <ƛ n. (plus b n)> 0)
+//  = (a (plus b) 0)
 export function mult_(a: $Numeral, b: $Numeral): $Numeral {
   return a((n) => plus(b, n), $0);
 }
 
-// power = ƛ a e. ƛ f x. ((e <ƛ n. ƛ x.(a n x)> f) x)
-// = ƛ a e. ƛ f x. ((e a f) x)
-// = ƛ a e. e a
+//  power = ƛ a e. ƛ f x. ((e <ƛ n. ƛ x.(a n x)> f) x)
+//  = ƛ a e. ƛ f x. ((e a f) x)
+//  = ƛ a e. e a
 export function pow(a: $Numeral, e: $Numeral): $Numeral {
   return function (f, x) {
     type T = typeof x;
@@ -76,13 +76,24 @@ export function pow(a: $Numeral, e: $Numeral): $Numeral {
   };
 }
 
-// power = ƛ a e. (e <ƛ n. (mult a n)> 1)
-// = ƛ a e. (e (mult a) 1)
+//  power = ƛ a e. (e <ƛ n. (mult a n)> 1)
+//  = ƛ a e. (e (mult a) 1)
 export function pow_(a: $Numeral, e: $Numeral): $Numeral {
   return e((n) => mult(a, n), $1);
 }
 
-// predecessor = ƛ n. ƛ f x. (second (n <ƛ p. (pair false (((first p) <ƛ x. x> f) (second p)))> (pair true x)))
+//  predecessor = ƛ n. ƛ f x. (second (n <ƛ p. (pair false (((first p) <ƛ x. x> f) (second p)))> (pair true x)))
+//  = ƛ n. ƛ f x. ()
+export function pred(n: $Numeral): $Numeral {}
+
+//  predecessor = ƛ n. ƛ f x.
+//    (second (n
+//      <ƛ p.
+//        (pair
+//          false
+//          (((first p) <ƛ x. x> f)
+//            (second p)))>
+//      (pair true x)))
 export function pred_(n: $Numeral): $Numeral {
   return function (f, x) {
     type T = typeof x;
